0c8054eb21e5012e465d1b2f3a05026c9d825533
katsenkatorz <>
2023-08-03 14:21:00 +0000

    Updates

---
+++ _next/static/ba-q1sKnvqhlF5MzQAsij/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5de898299e07617d.css","static/chunks/pages/index-4e2534fb783bbff6.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
